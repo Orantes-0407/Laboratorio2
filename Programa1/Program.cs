6d00efabf1b96736452ba033cc553099974870d2
Melany Rosmery Pérez Orantes <mperezo37@miumg.edu.gt>
2025-02-12 23:45:39 -0600

    Add files via upload

---
+++ Programa1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+string num1, num2;
+int resultado;
+Console.WriteLine("ingrese numero1");
+num1= Console.ReadLine();
+Console.WriteLine("ingrese numero2:");
+num2= Console.ReadLine();
+Console.Writeline("El resultado de la suma es: " +resultado);
+try
+{
+    resultado = int.Parse(num1) + int.Parse(num2);
+    catch(Exception error){
+        Console.WriteLine("Ups, ingresa numeros validos");
+
+    }
+Console.WriteLine("fin del programa");
+  
+
+
